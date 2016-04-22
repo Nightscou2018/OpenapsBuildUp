7fadb82bf84416c8e1c170e8b056a3312d8c72ed
mikestebbins <michaelstebbins@gmail.com>
2016-04-21 23:35:19 -0700

    work on sms send

--- twilio_sms_send_test.py
+++ twilio_sms_send_test.py
@@ -52,9 +52,44 @@ try:  # parse the enact/suggested file and see if there is a duration in there
 #------------------------------------------------------------------------------------------------------
 # Pseudocode for reservoir status SMS
 
-# Try to grab reservoir level
-# Figure out time and day of the week right NOW
+from datetime import datetime
+from datetime import time
+import json
+import subprocess
+
+VOLTAGE_THRESHOLD = 1.36
+SCHOOLNIGHT_TIME_THRESHOLD = (20,0)
+RESERVOIR_WARN = 30
+RESERVOIR_URGENT = 15
+
+try:  # open the reservoir level file
+    with open('monitor/reservoir.json') as data_file:    
+        reservoir_level = json.load(data_file)
+    voltage = battery_level["voltage"]
+except:
+    print ("error loading monitor/battery.json")
+
+# Figure out time and day of the week right NOW (http://stackoverflow.com/questions/415511/how-to-get-current-time-in-python, 
+# http://stackoverflow.com/questions/1831410/python-time-comparison)
+now = datetime.now()
+now_time = now.time()
+weekday = now.weekday()  # Get day of the week of Now (Return the day of the week as an integer, where Monday is 0 and Sunday is 6.)
+
 # If it's (past 8pm the night before a work day and the reservoir is < 30 units) or (the reservoir is < 15 units)
+if (weekday >= 0 and weekday < 4) or (weekday == 6):  # School nights
+    if now_time >= time(SCHOOLNIGHT_TIME_THRESHOLD):
+        if reservoir_level <= RESERVOIR_WARN:
+            print('yep-res_warn')
+        else:
+            print('nope-res_warn')
+    else:
+        print('nope-time')
+else:
+    print('nope-weekday')
+
+if reservoir_level <= RESERVOIR_URGENT:
+    print('yep-res_urg')
+
     # if there is a reservoir sent notification file present
         # if so, see how much time has elapsed since last SMS sent
             # if it's been more than YY hours
@@ -65,6 +100,13 @@ try:  # parse the enact/suggested file and see if there is a duration in there
         # send an SMS and create/update the reservoir sent notification file
 
 # Try to grab battery voltage 
+try:  # open the reservoir level file
+    with open('monitor/battery.json') as data_file:    
+        battery_level = json.load(data_file)
+    voltage = battery_level["voltage"]
+except:
+    print ("error loading monitor/battery.json")
+
 # If the battery voltage is less than 1.28 Volts ??
     # if there is a battery sent notification file present
         # if so, see how much time has elapsed since last SMS sent
@@ -78,13 +120,7 @@ try:  # parse the enact/suggested file and see if there is a duration in there
 
 
 #------------------------------------------------------------------------------------------------------
-# Write the code HERE:
-
-import datetime
-
-# Get day of the week of Now (Return the day of the week as an integer, where Monday is 0 and Sunday is 6.)
+# OLD
 time_now = datetime.datetime.today()
 day_of_the_week = time_now.weekday()
 
-# http://stackoverflow.com/questions/415511/how-to-get-current-time-in-python
-# http://stackoverflow.com/questions/1831410/python-time-comparison
