4785c4185ceb776e66762f8e2430141eaf3a997d
mikestebbins <michaelstebbins@gmail.com>
2016-05-23 20:45:09 -0700

    playing with nightscout api and python

--- nightscout_api_get_BGs.py
+++ nightscout_api_get_BGs.py
@@ -29,12 +29,20 @@ import pprint
 base_URL = 'https://mikestebbinscgmtest.azurewebsites.net/api/v1/'
 
 
-
+# example request for two days' values from README of nightcout
 # http://localhost:1337/api/v1/entries/sgv.json?find[dateString][$gte]=2015-08-28&find[dateString][$lte]=2015-08-30
 
 
 # r = requests.get('https://mikestebbinscgmtest.azurewebsites.net/api/v1/treatments?count=2')
-url = base_URL + 'treatments?count=2'
+# url = base_URL + 'entries/sgv'
+# url = base_URL + 'entries/sgv.json?find[dateString][$gte]=2016-05-08&find[dateString][$lte]=2016-05-09'
+
+#url = 'https://mikestebbinscgmtest.azurewebsites.net/api/v1/entries?find=[dateString][$gte]=2016-05-18'
+#url='https://mikestebbinscgmtest.azurewebsites.net/api/v1/entries/sgv?count=100'
+url='https://mikestebbinscgmtest.azurewebsites.net/api/v1/entries/sgv?find=%5Bdatestring%5D%5B%24gte%5D%3D2016-05-10'
+
+print (url)
+
 r = requests.get(url)
 
 print(r.status_code)
@@ -49,9 +57,9 @@ print()
 print(r.text)
 print()
 print()
-print(r.json())
+#print(r.json())
 print()
 print()
 
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(r.json())
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(r.json())
